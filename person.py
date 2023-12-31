4e0936f629b01f05f1e78c38ae2a57b51e6cdf24
Miguel Estêvão <miguelmariaestevao9@gmail.com>
2023-12-31 16:23:52 +0000

    treino de property

--- person.py
+++ person.py
@@ -2,20 +2,28 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
+    @property
     def display(self):
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
 
-class Student(Person):
-    def __init__(self, name, age, section):
-        super().__init__(name, age)
-        self.section = section
+    @property
+    def email(self):
+        print(f"{self.name}@gmail.com")
+    @email.setter
+    def email(self,new_email):
+        name = new_email.split("@")
+        self.name = name[0]
 
-    def display(self):
-        print(f"Name: {self.name}")
-        print(f"Age: {self.age}")
-        print(f"Section: {self.section}")
 
-Miguel = Student("Miguel", 20, "Mathematics")
-Miguel.display()
+
+Miguel = Person("Miguel", 20)
+
+
+Miguel.email = "Bruno@gmail.com"
+
+print(Miguel.name)
+print(Miguel.display)
+print(Miguel.email)
+
+
